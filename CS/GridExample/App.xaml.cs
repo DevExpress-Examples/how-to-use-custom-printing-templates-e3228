c3bc4b5ae3c5339cb5fbaf5d48e8e50e4b40c5bd
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/GridExample/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿// Developer Express Code Central Example:
+// How to use custom printing templates
+// 
+// This example demonstrates how to use grid's printing templates to change the
+// grid printing appearance.
+// 
+// You can find sample updates and versions for different programming languages here:
+// http://www.devexpress.com/example=E3228
+
+// Developer Express Code Central Example:
+// How to use custom printing templates
+// 
+// This example demonstrates how to use grid's printing templates to change the
+// grid printing appearance.
+// 
+// You can find sample updates and versions for different programming languages here:
+// http://www.devexpress.com/example=E3228
+
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace GridExample {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application {
+    }
+}
